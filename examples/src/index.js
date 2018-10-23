521ffa73977d3e92c15e01740e848c3071684120
David DeArmon <david.dearmon@gmail.com>
2018-10-23 15:52:15 -0500

    implemented hot-loader

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render} from 'react-dom';
-import MyComponent from '../../src';
+import MyComponent from '../../src/index.js';
+import { AppContainer } from 'react-hot-loader';
 const App = () => (
     <MyComponent />
 );
-render(<App />, document.getElementById("root"));
+render(<AppContainer><App /></AppContainer>, document.getElementById("root"));
+module.hot.accept('../../src/index.js',()=>{
+    const NextApp = require('../../src/index.js').default;
+    render(<NextApp/>,document.getElementById("root"))
+})
